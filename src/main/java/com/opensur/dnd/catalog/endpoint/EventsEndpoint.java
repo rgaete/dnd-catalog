6cbefa2cbf65659e86b29234d3d7d529b92b014b
Ricardo Gaete Duarte <ricardo.gaete@lan.com>
2017-05-17 01:52:40 -0400

    Adding some prototypes for our contracts

--- src/main/java/com/opensur/dnd/catalog/endpoint/EventsEndpoint.java
+++ src/main/java/com/opensur/dnd/catalog/endpoint/EventsEndpoint.java
@@ -3,7 +3,6 @@ package com.opensur.dnd.catalog.endpoint;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CatalogEndpoint {
-
+public class EventsEndpoint {
 
 }
